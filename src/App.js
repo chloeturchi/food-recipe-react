706d1112fbb599c45b5a495d222348ad5bc1fdac
Chloe <chloeturchi@gmail.com>
2020-06-30 12:25:44 +0200

    add msg alert empty form

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import PropTypes from 'prop-types'
 import Axios from 'axios';
 import {v4 as uuidv4} from 'uuid'
 import Recipe from './components/Recipe'
+import Alert from './components/Alert'
 import './App.css'
 
 const App = props => {
     const [query, setQuery] = useState("");
     const [recipes, setRecipes] = useState([]); 
+    const [alert, setAlert] = useState("");
 
     const APP_ID = "e90b429d";
     const APP_KEY = "2b16b7dbc664bd6cc8fd2c096013696d";
     const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
     const getData = async () => {
-        const result = await Axios.get(url);
-        setRecipes(result.data.hits)
-        console.log(result);
-        setQuery("");
+        if (query !== ""){
+            const result = await Axios.get(url);
+            setRecipes(result.data.hits)
+            console.log(result);
+            setQuery("");
+        } else {
+            setAlert('Please fill the form')
+        }
     }
 
     const onChange = e => {
@@ -35,6 +41,7 @@ const App = props => {
             <form 
                 className="search-form"
                 onSubmit={onSubmit}>
+                {alert !== "" && <Alert alert={alert}/>}
                 <input 
                     type="text" 
                     placeholder="Search Food"
