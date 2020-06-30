b7462146993ac966c7be6f865463a5ce51a0f195
Chloe <chloeturchi@gmail.com>
2020-06-30 11:31:39 +0200

    add axios, usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios';
 import './App.css'
+import Axios from 'axios';
 
 const App = props => {
+    const [query, setQuery] = useState("");
+
+    const APP_ID = "e90b429d";
+    const APP_KEY = "2b16b7dbc664bd6cc8fd2c096013696d";
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+
+    const getData = async () => {
+        const result = await Axios.get(url);
+        console.log(result)
+    }
+
+    const onChange = e => {
+        setQuery(e.target.value);
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        getData()
+    }
+
     return (
-        <div>
+        <div className="App">
             <h1>Food Searching App</h1>
+            <form 
+                className="search-form"
+                onSubmit={onSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Search Food"
+                    autoComplete="off"
+                    onChange={onChange} />
+                <input 
+                    type="submit" 
+                    value="search"/>
+            </form>
         </div>
     )
 }
